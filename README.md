10b9747c5f748b1280354aa23b73721a317055b2
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2021-01-21 19:32:40 +0200

    changed django-heroku to django-on-heroku

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # WebMark
 Web platform for benchmarking quantum computing algorithms
 
+## Django - structure
+
+WebMark = Project
+WebCLI = Application
+
+Requirements: django, django-on-heroku, gunicorn
+
+
--- WebMark/settings.py
+++ WebMark/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,4 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
