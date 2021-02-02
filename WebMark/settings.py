7cbe63145cf9ec746001a96668b18e615df4b0a6
Tapani Honkanen <tapani.honkanen@hotmail.com>
2021-02-02 10:58:34 +0200

    Changed login redirect url to home

--- WebMark/settings.py
+++ WebMark/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
