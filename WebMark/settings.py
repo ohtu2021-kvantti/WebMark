25456097171e0121a10fc17ec1930cc0f4dec4b8
Akira Taguchi <akirahattivatti@gmail.com>
2021-02-09 09:53:25 +0200

    ghub actions trigger 8

--- WebMark/settings.py
+++ WebMark/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': os.getenv("DATABASE_USER"),
         'PASSWORD': os.getenv("DATABASE_PASSWORD"),
         'HOST': os.getenv("DATABASE_HOST"),
-        'PORT': os.getenv("DATABASE_PORT", "5432"),
+        'PORT': os.getenv("DATABASE_PORT", "5431"),
     }
 }
 
