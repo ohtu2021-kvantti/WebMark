1bcb8ff690d7054f90f2d7008866337d42ec8363
Tapani Honkanen <tapani.honkanen@hotmail.com>
2021-02-14 16:39:19 +0200

    Added tests

--- WebCLI/tests.py
+++ WebCLI/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase, Client
+from django.test import SimpleTestCase, TestCase, Client
 from django.contrib.auth.models import User
 
 from .db import new_algorithm, new_algorithm_type, new_molecule, save_metrics
 from .db import get_algorithm_types, get_molecules, get_public_algorithms
+from django.urls import reverse
 
 
 class DatabaseTest(TestCase):
@@ -70,3 +71,57 @@ class WebFunctionTest(TestCase):
         response = c.get('/algorithm/?index=1')
         self.assertEqual(response.status_code, 403)
 """
+
+
+class AlgorithmComparisonTest(SimpleTestCase):
+
+    def setUp(self):
+        user = User.objects.create_user("testuser", "test@example.com", "secret")
+        self.client.login(username="testuser", password="testuserpassword")
+        other_user = User.objects.create_user("otheruser", "other@example.com", "secret")
+        algorithm_type = new_algorithm_type("VQE")
+        molecule = new_molecule("water", "H2O")
+        self.my_algorithm = new_algorithm(
+            user, "my algo", algorithm_type, "execute()", molecule, public=True
+        )
+        self.my_private_algorithm = new_algorithm(
+            user, "my private algo", algorithm_type, "execute()", molecule, public=False
+        )
+        self.other_user_private_algorithm = new_algorithm(
+            other_user, "private algo", algorithm_type, "exec()", molecule, public=False
+        )
+        self.other_user_public_algorithm = new_algorithm(
+            other_user, "public algo", algorithm_type, "exec()", molecule, public=True
+        )
+
+    def random_parameters_are_handled_correctly(self):
+        response = self.client.get("/compare/a/exec()")
+        self.assertRedirects(response, reverse("home"), status_code=302, target_status_code=200)
+
+    def cannot_compare_nonexistent_algorithms(self):
+        response = self.client.get("/compare/10/11")
+        self.assertRedirects(response, reverse("home"), status_code=302, target_status_code=200)
+
+    def cannot_compare_algorithm_to_itself(self):
+        response = self.client.get(
+            f"/compare/{self.my_algorithm.pk}/{self.my_algorithm.pk}"
+        )
+        self.assertRedirects(response, reverse("home"), status_code=302, target_status_code=200)
+
+    def can_compare_to_private_by_current_user(self):
+        response = self.client.get(
+            f"/compare/{self.my_algorithm.pk}/{self.my_private_algorithm.pk}"
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def cannot_compare_to_private_algorithm_by_other_user(self):
+        response = self.client.get(
+            f"/compare/{self.my_algorithm.pk}/{self.other_user_private_algorithm.pk}"
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def can_compare_to_public_algorithm_by_other_user(self):
+        response = self.client.get(
+            f"/compare/{self.my_algorithm.pk}/{self.other_user_public_algorithm.pk}"
+        )
+        self.assertEqual(response.status_code, 200)
