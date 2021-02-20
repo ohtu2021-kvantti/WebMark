7c7f80b5b66ed49b56ee4dde093d829521024a47
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2021-02-20 14:21:12 +0200

    Even more tests

--- WebCLI/tests.py
+++ WebCLI/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from .models import Molecule, Algorithm_type, Algorithm, Algorithm_version
 import datetime
+import pytz
+
 
 class WebFunctionTestLogin(TestCase):
     def test_signup(self):
@@ -86,9 +88,9 @@ class WebFunctionTestAddDataAsUser(TestCase):
                      'algorithm': 'exec()',
                      'article_link': 'https://kela.fi',
                      'github_link': 'https://vn.fi'})
-        self.c.post('/addVersion/?index='+str(user_id),
-                    {'algorithm': 'print(1)\nexec()'})
         a = Algorithm.objects.get(name='test_algorithm')
+        self.c.post('/addVersion/?index='+str(a.pk),
+                    {'algorithm': 'print(1)\nexec()'})
         v = Algorithm_version.objects.filter(algorithm_id=a)
 
         self.assertEqual(a.public, True)
@@ -126,6 +128,7 @@ class WebFunctionTestMyAlgorithmsView(TestCase):
         response = self.client.get("/myAlgorithms/")
         self.assertEqual(response.status_code, 200)
 
+
 class WebFunctionTestViewData(TestCase):
 
     @classmethod
@@ -146,41 +149,71 @@ class WebFunctionTestViewData(TestCase):
         at2.save()
         at3 = Algorithm_type(type_name='type3')
         at3.save()
-        a1 = Algorithm(name='Algo1', public=True, molecule=m1, algorithm_type=at1, user=u1, article_link='https://alink1.com', github_link='https://gtlink1.com')
+        a1 = Algorithm(name='Algo1', public=True, molecule=m1, algorithm_type=at1, user=u1,
+                       article_link='https://alink1.com', github_link='https://gtlink1.com')
         a1.save()
-        a2 = Algorithm(name='Algo2', public=False, molecule=m1, algorithm_type=at2, user=u1, article_link='https://alink2.com', github_link='https://gtlink2.com')
+        a2 = Algorithm(name='Algo2', public=False, molecule=m1, algorithm_type=at2, user=u1,
+                       article_link='https://alink2.com', github_link='https://gtlink2.com')
         a2.save()
-        a3 = Algorithm(name='Algo3', public=True, molecule=m2, algorithm_type=at1, user=u1, article_link='https://alink4.com', github_link='https://gtlink3.com')
+        a3 = Algorithm(name='Algo3', public=True, molecule=m2, algorithm_type=at1, user=u1,
+                       article_link='https://alink3.com', github_link='https://gtlink3.com')
         a3.save()
-        a4 = Algorithm(name='Algo4', public=True, molecule=m3, algorithm_type=at2, user=u2, article_link='https://alink4.com', github_link='https://gtlink4.com')
+        a4 = Algorithm(name='Algo4', public=True, molecule=m3, algorithm_type=at2, user=u2,
+                       article_link='https://alink4.com', github_link='https://gtlink4.com')
         a4.save()
-        a5 = Algorithm(name='Algo5', public=False, molecule=m2, algorithm_type=at3, user=u2, article_link='https://alink5.com', github_link='https://gtlink5.com')
+        a5 = Algorithm(name='Algo5', public=False, molecule=m2, algorithm_type=at3, user=u2,
+                       article_link='https://alink5.com', github_link='https://gtlink5.com')
         a5.save()
-        a6 = Algorithm(name='Algo6', public=False, molecule=m1, algorithm_type=at2, user=u2, article_link='https://alink6.com', github_link='https://gtlink2.com')
+        a6 = Algorithm(name='Algo6', public=False, molecule=m1, algorithm_type=at2, user=u2,
+                       article_link='https://alink6.com', github_link='https://gtlink6.com')
         a6.save()
-        av = Algorithm_version(algorithm_id=a1, timestamp=datetime.datetime(2021, 2, 10, 10, 0), algorithm='algorithm1\nversion1\n')
+        av = Algorithm_version(algorithm_id=a1,
+                               timestamp=datetime.datetime(2021, 2, 10, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm1\nversion1\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a1, timestamp=datetime.datetime(2021, 2, 10, 11, 0), algorithm='algorithm1\nversion2\n')
+        av = Algorithm_version(algorithm_id=a1,
+                               timestamp=datetime.datetime(2021, 2, 10, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm1\nversion2\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a2, timestamp=datetime.datetime(2021, 2, 11, 10, 0), algorithm='algorithm2\nversion1\n')
+        av = Algorithm_version(algorithm_id=a2,
+                               timestamp=datetime.datetime(2021, 2, 11, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm2\nversion1\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a2, timestamp=datetime.datetime(2021, 2, 11, 11, 0), algorithm='algorithm2\nversion2\n')
+        av = Algorithm_version(algorithm_id=a2,
+                               timestamp=datetime.datetime(2021, 2, 11, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm2\nversion2\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a3, timestamp=datetime.datetime(2021, 2, 12, 10, 0), algorithm='algorithm3\nversion1\n')
+        av = Algorithm_version(algorithm_id=a3,
+                               timestamp=datetime.datetime(2021, 2, 12, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm3\nversion1\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a3, timestamp=datetime.datetime(2021, 2, 15, 11, 0), algorithm='algorithm3\nversion2\n')
+        av = Algorithm_version(algorithm_id=a3,
+                               timestamp=datetime.datetime(2021, 2, 15, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm3\nversion2\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a4, timestamp=datetime.datetime(2021, 2, 17, 10, 0), algorithm='algorithm4\nversion1\n')
+        av = Algorithm_version(algorithm_id=a4,
+                               timestamp=datetime.datetime(2021, 2, 17, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm4\nversion1\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a4, timestamp=datetime.datetime(2021, 2, 18, 11, 0), algorithm='algorithm4\nversion2\n')
+        av = Algorithm_version(algorithm_id=a4,
+                               timestamp=datetime.datetime(2021, 2, 18, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm4\nversion2\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a5, timestamp=datetime.datetime(2021, 2, 17, 18, 0), algorithm='algorithm5\nversion1\n')
+        av = Algorithm_version(algorithm_id=a5,
+                               timestamp=datetime.datetime(2021, 2, 17, 18, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm5\nversion1\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a5, timestamp=datetime.datetime(2021, 2, 18, 12, 0), algorithm='algorithm5\nversion2\n')
+        av = Algorithm_version(algorithm_id=a5,
+                               timestamp=datetime.datetime(2021, 2, 18, 12, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm5\nversion2\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a6, timestamp=datetime.datetime(2021, 2, 17, 9, 0), algorithm='algorithm6\nversion1\n')
+        av = Algorithm_version(algorithm_id=a6,
+                               timestamp=datetime.datetime(2021, 2, 17, 9, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm6\nversion1\n')
         av.save()
-        av = Algorithm_version(algorithm_id=a6, timestamp=datetime.datetime(2021, 2, 18, 15, 0), algorithm='algorithm6\nversion2\n')
+        av = Algorithm_version(algorithm_id=a6,
+                               timestamp=datetime.datetime(2021, 2, 18, 15, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm6\nversion2\n')
         av.save()
 
     def setUp(self):
@@ -212,7 +245,10 @@ class WebFunctionTestViewData(TestCase):
         a = Algorithm.objects.get(name='Algo1')
         v = Algorithm_version.objects.filter(algorithm_id=a)[0]
         self.client.post('/addMetrics/?index=' + str(v.pk),
-               {'iterations': '1', 'measurements': '2', 'circuit_depth': '4', 'accuracy': '5.2'})
+                         {'iterations': '1',
+                          'measurements': '2',
+                          'circuit_depth': '4',
+                          'accuracy': '5.2'})
         v2 = Algorithm_version.objects.get(pk=v.pk)
         self.assertEqual(v2.iterations, 1)
         self.assertEqual(v2.measurements, 2)
@@ -223,7 +259,10 @@ class WebFunctionTestViewData(TestCase):
         a = Algorithm.objects.get(name='Algo2')
         v = Algorithm_version.objects.filter(algorithm_id=a)[0]
         self.client.post('/addMetrics/?index=' + str(v.pk),
-               {'iterations': '1', 'measurements': '2', 'circuit_depth': '4', 'accuracy': '5.2'})
+                         {'iterations': '1',
+                          'measurements': '2',
+                          'circuit_depth': '4',
+                          'accuracy': '5.2'})
         v2 = Algorithm_version.objects.get(pk=v.pk)
         self.assertEqual(v2.iterations, 1)
         self.assertEqual(v2.measurements, 2)
@@ -234,7 +273,10 @@ class WebFunctionTestViewData(TestCase):
         a = Algorithm.objects.get(name='Algo4')
         v = Algorithm_version.objects.filter(algorithm_id=a)[0]
         self.client.post('/addMetrics/?index=' + str(v.pk),
-               {'iterations': '1', 'measurements': '2', 'circuit_depth': '4', 'accuracy': '5.2'})
+                         {'iterations': '1',
+                          'measurements': '2',
+                          'circuit_depth': '4',
+                          'accuracy': '5.2'})
         v2 = Algorithm_version.objects.get(pk=v.pk)
         self.assertEqual(v2.iterations, None)
 
@@ -242,7 +284,10 @@ class WebFunctionTestViewData(TestCase):
         a = Algorithm.objects.get(name='Algo5')
         v = Algorithm_version.objects.filter(algorithm_id=a)[0]
         self.client.post('/addMetrics/?index=' + str(v.pk),
-               {'iterations': '1', 'measurements': '2', 'circuit_depth': '4', 'accuracy': '5.2'})
+                         {'iterations': '1',
+                          'measurements': '2',
+                          'circuit_depth': '4',
+                          'accuracy': '5.2'})
         v2 = Algorithm_version.objects.get(pk=v.pk)
         self.assertEqual(v2.iterations, None)
 
@@ -250,13 +295,69 @@ class WebFunctionTestViewData(TestCase):
         a = Algorithm.objects.get(name='Algo3')
         v = Algorithm_version.objects.filter(algorithm_id=a)[0]
         self.client.post('/addMetrics/?index=' + str(v.pk),
-               {'iterations': '1', 'measurements': '', 'circuit_depth': '', 'accuracy': ''})
+                         {'iterations': '1',
+                          'measurements': '',
+                          'circuit_depth': '',
+                          'accuracy': ''})
         v2 = Algorithm_version.objects.get(pk=v.pk)
         self.assertEqual(v2.iterations, 1)
         self.assertEqual(v2.measurements, None)
         self.assertEqual(v2.circuit_depth, None)
         self.assertEqual(v2.accuracy, None)
-        
 
+    def test_add_no_metrics(self):
+        a = Algorithm.objects.get(name='Algo3')
+        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
+        self.client.post('/addMetrics/?index=' + str(v.pk),
+                         {'iterations': '',
+                          'measurements': '',
+                          'circuit_depth': '',
+                          'accuracy': ''})
+        v2 = Algorithm_version.objects.get(pk=v.pk)
+        self.assertEqual(v2.iterations, None)
+        self.assertEqual(v2.measurements, None)
+        self.assertEqual(v2.circuit_depth, None)
+        self.assertEqual(v2.accuracy, None)
 
+    def test_add_metrics_view(self):
+        a = Algorithm.objects.get(name='Algo3')
+        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
+        response = str(self.client.get('/addMetrics/?index=' + str(v.pk)).content)
+        self.assertFalse(response.find('Algo3') < 0)
 
+    def test_add_other_user_metrics_view(self):
+        a = Algorithm.objects.get(name='Algo4')
+        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
+        response = self.client.get('/addMetrics/?index=' + str(v.pk))
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_new_version_to_other_user_algorithm(self):
+        a = Algorithm.objects.get(name='Algo4')
+        response = self.client.post('/addVersion/?index='+str(a.pk),
+                                    {'algorithm': 'print(1)\nexec()'})
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_new_version_view(self):
+        a = Algorithm.objects.get(name='Algo3')
+        response = str(self.client.get('/addVersion/?index='+str(a.pk)).content)
+        self.assertFalse(response.find('Algo3') < 0)
+        self.assertFalse(response.find('version2') < 0)
+
+    def test_algorithm_details_view(self):
+        a = Algorithm.objects.get(name='Algo3')
+        response = str(self.client.get('/algorithm/'+str(a.pk)).content)
+        self.assertFalse(response.find('Algo3') < 0)
+        self.assertFalse(response.find('https://alink3.com') < 0)
+        self.assertFalse(response.find('https://gtlink3.com') < 0)
+
+    def test_algorithm_details_other_user_private_view(self):
+        a = Algorithm.objects.get(name='Algo5')
+        response = self.client.get('/algorithm/'+str(a.pk))
+        self.assertEqual(response.status_code, 403)
+
+    def test_algorithm_details_other_user_public_view(self):
+        a = Algorithm.objects.get(name='Algo4')
+        response = str(self.client.get('/algorithm/'+str(a.pk)).content)
+        self.assertFalse(response.find('Algo4') < 0)
+        self.assertFalse(response.find('https://alink4.com') < 0)
+        self.assertFalse(response.find('https://gtlink4.com') < 0)
