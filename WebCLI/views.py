909b403ac42d5c47cbea20c71dbdfec974fc3b2e
Tapani Honkanen <tapani.honkanen@hotmail.com>
2021-02-20 14:21:06 +0200

    Fixed linting errors

--- WebCLI/views.py
+++ WebCLI/views.py
@@ -37,8 +37,8 @@ class AlgorithmTable(Table):
     name = Column(linkify=True)
     github_link = Column(verbose_name='Github')
     article_link = Column(verbose_name='Article')
-    pk = TemplateColumn(verbose_name='Compare', 
-    template_name="WebCLI/compare.html", orderable=False)
+    pk = TemplateColumn(verbose_name='Compare',
+                        template_name="WebCLI/compare.html", orderable=False)
 #    timestamp = DateTimeColumn(format='d.m.Y', verbose_name='Date')
 
     class Meta:
@@ -46,7 +46,7 @@ class AlgorithmTable(Table):
         exclude = ('id', 'public')
         attrs = {'class': 'table table-hover table-sm'}
         row_attrs = {
-            "id": lambda record : (record.pk)
+            "id": lambda record: (record.pk)
         }
 
     def render_github_link(self, value):
@@ -56,10 +56,8 @@ class AlgorithmTable(Table):
         return format_html(f'<a href={value}>Article</a>')
 
     def render_compareTo(self, value):
-        
-        return format_html(f'<input type="checkbox">')
-    
-    
+        return format_html('<input type="checkbox">')
+
 
 class AlgorithmListView(SingleTableMixin, FilterView):
     model = Algorithm
@@ -232,7 +230,8 @@ def compare_algorithms(request, a1_id, a2_id):
         return redirect("home")
 
     queryset = Algorithm.objects.filter(pk=a1_id) | Algorithm.objects.filter(pk=a2_id)
-    queryset2 = Algorithm_version.objects.filter(pk=a1_id) | Algorithm_version.objects.filter(pk=a2_id)
+    queryset2 = Algorithm_version.objects.filter(pk=a1_id) | \
+        Algorithm_version.objects.filter(pk=a2_id)
     if len(queryset) != 2:  # check that we have found two unique algorithms
         return redirect("home")
 
@@ -241,14 +240,17 @@ def compare_algorithms(request, a1_id, a2_id):
     # simple barchart data
     (av1, av2) = queryset2
     (a1, a2) = queryset
-    algo_data = [["Algorithm comparison", a1.name, a2.name], ["Iterations", av1.iterations, av2.iterations], 
-    ["Measurements", av1.measurements, av2.measurements], ["Circuit depth", av1.circuit_depth, av2.circuit_depth], ["Accuracy", av1.accuracy, av2.accuracy]]
-    
+    algo_data = [["Algorithm comparison", a1.name, a2.name],
+                 ["Iterations", av1.iterations, av2.iterations],
+                 ["Measurements", av1.measurements, av2.measurements],
+                 ["Circuit depth", av1.circuit_depth, av2.circuit_depth],
+                 ["Accuracy", av1.accuracy, av2.accuracy]]
+
     if not a1.public and request.user.pk != a1.user.pk:
         raise PermissionDenied
     if not a2.public and request.user.pk != a2.user.pk:
         raise PermissionDenied
 
     return render(request, 'WebCLI/compareAlgorithms.html',
-                  {'a1': a1, 'av1': av1, 'a2': a2, 'av2': av2, 'graph_data': graph_data, 'algo_data': algo_data})
-    
+                  {'a1': a1, 'av1': av1, 'a2': a2, 'av2': av2,
+                   'graph_data': graph_data, 'algo_data': algo_data})
