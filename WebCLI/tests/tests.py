715175093a3628c01926a85bb6126bfb55601eb0
Akseli N <virtualakseli@gmail.com>
2021-03-10 09:15:08 +0200

    Test-classes not over length limits anymore

--- WebCLI/tests/tests.py
+++ WebCLI/tests/tests.py
@@ -227,6 +227,171 @@ class WebFunctionTestViewData(TestCase):
         self.assertFalse(response.find('Algo4') < 0)
         self.assertTrue(response.find('Algo5') < 0)
 
+    def test_add_metrics_view(self):
+        a = Algorithm.objects.get(name='Algo3')
+        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
+        response = str(self.client.get('/addMetrics/?index=' + str(v.pk)).content)
+        self.assertFalse(response.find('Algo3') < 0)
+
+    def test_add_other_user_metrics_view(self):
+        a = Algorithm.objects.get(name='Algo4')
+        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
+        response = self.client.get('/addMetrics/?index=' + str(v.pk))
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_new_version_to_other_user_algorithm(self):
+        a = Algorithm.objects.get(name='Algo4')
+        response = self.client.post('/addVersion/?index='+str(a.pk),
+                                    {'algorithm': 'print(1)\nexec()'})
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_new_version_view(self):
+        a = Algorithm.objects.get(name='Algo3')
+        response = str(self.client.get('/addVersion/?index='+str(a.pk)).content)
+        self.assertFalse(response.find('Algo3') < 0)
+        self.assertFalse(response.find('version2') < 0)
+
+    def test_algorithm_details_view(self):
+        a = Algorithm.objects.get(name='Algo3')
+        response = str(self.client.get('/algorithm/'+str(a.pk)).content)
+        self.assertFalse(response.find('Algo3') < 0)
+        self.assertFalse(response.find('https://alink3.com') < 0)
+        self.assertFalse(response.find('https://gtlink3.com') < 0)
+
+    def test_algorithm_details_other_user_private_view(self):
+        a = Algorithm.objects.get(name='Algo5')
+        response = self.client.get('/algorithm/'+str(a.pk))
+        self.assertEqual(response.status_code, 403)
+
+    def test_algorithm_details_other_user_public_view(self):
+        a = Algorithm.objects.get(name='Algo4')
+        response = str(self.client.get('/algorithm/'+str(a.pk)).content)
+        self.assertFalse(response.find('Algo4') < 0)
+        self.assertFalse(response.find('https://alink4.com') < 0)
+        self.assertFalse(response.find('https://gtlink4.com') < 0)
+
+    def test_update_details(self):
+        a = Algorithm.objects.get(name='Algo1')
+        self.client.post('/updateAlgorithm/?index='+str(a.pk),
+                         {'name': 'Algo1',
+                          'algorithm_type': Algorithm_type.objects.get(type_name='type1').pk,
+                          'public': 'on',
+                          'article_link': 'https://aalink1.com',
+                          'github_link': 'https://gtlink1.com',
+                          'user': User.objects.get(username='Bob').pk})
+        a = Algorithm.objects.get(name='Algo1')
+        self.assertEqual(a.article_link, 'https://aalink1.com')
+
+    def test_update_other_user_details(self):
+        a = Algorithm.objects.get(name='Algo4')
+        response = self.client.post('/updateAlgorithm/?index='+str(a.pk),
+                                    {'name': 'Algo4',
+                                     'algorithm_type': a.algorithm_type.pk,
+                                     'public': 'on',
+                                     'article_link': 'https://aalink4.com',
+                                     'github_link': 'https://gtlink4.com',
+                                     'user': User.objects.get(username='Bob').pk})
+        self.assertEqual(response.status_code, 403)
+
+
+class WebFunctionTestMetrics(TestCase):
+
+    @classmethod
+    def setUpTestAlgorithms(self):
+        self.a1 = Algorithm(name='Algo1', public=True, algorithm_type=self.at1, user=self.u1,
+                       article_link='https://alink1.com', github_link='https://gtlink1.com')
+        self.a1.save()
+        self.a2 = Algorithm(name='Algo2', public=False, algorithm_type=self.at2, user=self.u1,
+                       article_link='https://alink2.com', github_link='https://gtlink2.com')
+        self.a2.save()
+        self.a3 = Algorithm(name='Algo3', public=True, algorithm_type=self.at1, user=self.u1,
+                       article_link='https://alink3.com', github_link='https://gtlink3.com')
+        self.a3.save()
+        self.a4 = Algorithm(name='Algo4', public=True, algorithm_type=self.at2, user=self.u2,
+                       article_link='https://alink4.com', github_link='https://gtlink4.com')
+        self.a4.save()
+        self.a5 = Algorithm(name='Algo5', public=False, algorithm_type=self.at3, user=self.u2,
+                       article_link='https://alink5.com', github_link='https://gtlink5.com')
+        self.a5.save()
+        self.a6 = Algorithm(name='Algo6', public=False, algorithm_type=self.at2, user=self.u2,
+                       article_link='https://alink6.com', github_link='https://gtlink6.com')
+        self.a6.save()
+
+    def setUpTestAlgorithmVersions(self):
+        self.av = Algorithm_version(algorithm_id=self.a1,
+                               timestamp=datetime.datetime(2021, 2, 10, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm1\nversion1\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a1,
+                               timestamp=datetime.datetime(2021, 2, 10, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm1\nversion2\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a2,
+                               timestamp=datetime.datetime(2021, 2, 11, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm2\nversion1\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a2,
+                               timestamp=datetime.datetime(2021, 2, 11, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm2\nversion2\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a3,
+                               timestamp=datetime.datetime(2021, 2, 12, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm3\nversion1\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a3,
+                               timestamp=datetime.datetime(2021, 2, 15, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm3\nversion2\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a4,
+                               timestamp=datetime.datetime(2021, 2, 17, 10, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm4\nversion1\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a4,
+                               timestamp=datetime.datetime(2021, 2, 18, 11, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm4\nversion2\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a5,
+                               timestamp=datetime.datetime(2021, 2, 17, 18, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm5\nversion1\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a5,
+                               timestamp=datetime.datetime(2021, 2, 18, 12, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm5\nversion2\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a6,
+                               timestamp=datetime.datetime(2021, 2, 17, 9, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm6\nversion1\n')
+        self.av.save()
+        self.av = Algorithm_version(algorithm_id=self.a6,
+                               timestamp=datetime.datetime(2021, 2, 18, 15, 0, 0, 0, pytz.UTC),
+                               algorithm='algorithm6\nversion2\n')
+        self.av.save()
+
+    @classmethod
+    def setUpTestData(self):
+        self.u1 = User.objects.create_user('Bob', 'bob@example.com', 'bobpassword')
+        self.u1.save()
+        self.u2 = User.objects.create_user('Alice', 'alice@example.com', 'alicepassword')
+        self.u2.save()
+        self.m1 = Molecule(name='molecule1', structure='structure1')
+        self.m1.save()
+        self.m2 = Molecule(name='molecule2', structure='structure2')
+        self.m2.save()
+        self.m3 = Molecule(name='molecule3', structure='structure3')
+        self.m3.save()
+        self.at1 = Algorithm_type(type_name='type1')
+        self.at1.save()
+        self.at2 = Algorithm_type(type_name='type2')
+        self.at2.save()
+        self.at3 = Algorithm_type(type_name='type3')
+        self.at3.save()
+        self.setUpTestAlgorithms()
+        
+        self.setUpTestAlgorithmVersions(self)
+    
+    def setUp(self):
+        self.client.login(username="Bob", password="bobpassword")
+
     def test_add_metrics(self):
         a = Algorithm.objects.get(name='Algo1')
         v = Algorithm_version.objects.filter(algorithm_id=a)[0]
@@ -378,69 +543,3 @@ class WebFunctionTestViewData(TestCase):
                                      'accuracy': 'aa'})
         self.assertEqual(response.status_code, 400)
         self.assertFalse(str(response.content).find('Metrics input needs to be numeric') < 0)
-
-    def test_add_metrics_view(self):
-        a = Algorithm.objects.get(name='Algo3')
-        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
-        response = str(self.client.get('/addMetrics/?index=' + str(v.pk)).content)
-        self.assertFalse(response.find('Algo3') < 0)
-
-    def test_add_other_user_metrics_view(self):
-        a = Algorithm.objects.get(name='Algo4')
-        v = Algorithm_version.objects.filter(algorithm_id=a)[0]
-        response = self.client.get('/addMetrics/?index=' + str(v.pk))
-        self.assertEqual(response.status_code, 403)
-
-    def test_add_new_version_to_other_user_algorithm(self):
-        a = Algorithm.objects.get(name='Algo4')
-        response = self.client.post('/addVersion/?index='+str(a.pk),
-                                    {'algorithm': 'print(1)\nexec()'})
-        self.assertEqual(response.status_code, 403)
-
-    def test_add_new_version_view(self):
-        a = Algorithm.objects.get(name='Algo3')
-        response = str(self.client.get('/addVersion/?index='+str(a.pk)).content)
-        self.assertFalse(response.find('Algo3') < 0)
-        self.assertFalse(response.find('version2') < 0)
-
-    def test_algorithm_details_view(self):
-        a = Algorithm.objects.get(name='Algo3')
-        response = str(self.client.get('/algorithm/'+str(a.pk)).content)
-        self.assertFalse(response.find('Algo3') < 0)
-        self.assertFalse(response.find('https://alink3.com') < 0)
-        self.assertFalse(response.find('https://gtlink3.com') < 0)
-
-    def test_algorithm_details_other_user_private_view(self):
-        a = Algorithm.objects.get(name='Algo5')
-        response = self.client.get('/algorithm/'+str(a.pk))
-        self.assertEqual(response.status_code, 403)
-
-    def test_algorithm_details_other_user_public_view(self):
-        a = Algorithm.objects.get(name='Algo4')
-        response = str(self.client.get('/algorithm/'+str(a.pk)).content)
-        self.assertFalse(response.find('Algo4') < 0)
-        self.assertFalse(response.find('https://alink4.com') < 0)
-        self.assertFalse(response.find('https://gtlink4.com') < 0)
-
-    def test_update_details(self):
-        a = Algorithm.objects.get(name='Algo1')
-        self.client.post('/updateAlgorithm/?index='+str(a.pk),
-                         {'name': 'Algo1',
-                          'algorithm_type': Algorithm_type.objects.get(type_name='type1').pk,
-                          'public': 'on',
-                          'article_link': 'https://aalink1.com',
-                          'github_link': 'https://gtlink1.com',
-                          'user': User.objects.get(username='Bob').pk})
-        a = Algorithm.objects.get(name='Algo1')
-        self.assertEqual(a.article_link, 'https://aalink1.com')
-
-    def test_update_other_user_details(self):
-        a = Algorithm.objects.get(name='Algo4')
-        response = self.client.post('/updateAlgorithm/?index='+str(a.pk),
-                                    {'name': 'Algo4',
-                                     'algorithm_type': a.algorithm_type.pk,
-                                     'public': 'on',
-                                     'article_link': 'https://aalink4.com',
-                                     'github_link': 'https://gtlink4.com',
-                                     'user': User.objects.get(username='Bob').pk})
-        self.assertEqual(response.status_code, 403)
