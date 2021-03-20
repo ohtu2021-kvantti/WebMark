c850df03904de3c7b69113acf0a9c300e482f612
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2021-03-20 12:29:16 +0200

    updated models.py

--- WebCLI/models.py
+++ WebCLI/models.py
@@ -12,6 +12,9 @@ class Algorithm_type(models.Model):
 class Molecule(models.Model):
     name = models.TextField()
     structure = models.TextField()
+    active_orbitals = models.TextField()
+    basis_set = models.TextField()
+    transformation = models.TextField()
 
     def __str__(self):
         return self.name
@@ -36,7 +39,9 @@ class Algorithm(models.Model):
 class Algorithm_version(models.Model):
     algorithm_id = models.ForeignKey(Algorithm, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
-    algorithm = models.TextField()
+    circuit = models.TextField()
+    optimizer_module = models.TextField(null=True, blank=True)
+    optimizer_method = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return str(self.timestamp)
@@ -50,6 +55,29 @@ class Metrics(models.Model):
     measurements = models.IntegerField(null=True, blank=True)
     circuit_depth = models.IntegerField(null=True, blank=True)
     accuracy = models.FloatField(null=True, blank=True)
+    in_analyzed_queue = models.BooleanField(default=False)
+    last_analyze_ok = models.BooleanField(default=False)
 
     def __str__(self):
         return self.molecule.name
+
+
+class Analyzed_results(models.Model):
+    metrics = models.ForeignKey(Metrics, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    qubit_count = models.IntegerField(null=True, blank=True)
+    gate_depth = models.IntegerField(null=True, blank=True)
+    average_iterations = models.FloatField(null=True, blank=True)
+    success_rate = models.FloatField(null=True, blank=True)
+
+
+class Average_history(models.Model):
+    analyzed_results = models.ForeignKey(Analyzed_results, on_delete=models.CASCADE)
+    data = models.FloatField(null=True, blank=True)
+    iteration_number = models.IntegerField(null=True, blank=True)
+
+
+class Accuracy_history(models.Model):
+    analyzed_results = models.ForeignKey(Analyzed_results, on_delete=models.CASCADE)
+    data = models.FloatField(null=True, blank=True)
+    iteration_number = models.IntegerField(null=True, blank=True)
