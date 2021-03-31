f807c92bbcfa7527d27988303d103b2431360565
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2021-03-31 11:58:25 +0300

    model_to_dict added

--- WebCLI/views/test_algorithm.py
+++ WebCLI/views/test_algorithm.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
 from ..models import Molecule, Algorithm_version
 from WebCLI.celery import celery_app
+from django.forms.models import model_to_dict
 
 def test_algorithm(request):
-    version = Algorithm_version.objects.get(pk=request.GET.get("version"))
-    molecule = Molecule.objects.get(pk=request.GET.get("molecule"))
-    celery_app.send_task("benchmark.benchmark_task", args=[version.circuit, molecule.structure])
+    version =  model_to_dict(Algorithm_version.objects.get(pk=request.GET.get("version")))
+    molecule =  model_to_dict(Molecule.objects.get(pk=request.GET.get("molecule")))
+    data = {'molecule': molecule, 'version': version}
+    celery_app.send_task("benchmark.benchmark_task", args=[data])
     print(version.circuit)
     print(molecule.structure)
     return redirect('/myAlgorithms/')
