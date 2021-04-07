e7949b0e903de0744a483ecb1f586e82246ae336
Akseli N <virtualakseli@gmail.com>
2021-04-07 10:39:04 +0300

    Average history init
    Co-authored-by: Akira Taguchi <akirahattivatti@gmail.com>
    Co-authored-by: Sonja Vuorinen <sonja.vuorinen@helsinki.fi>

--- WebCLI/views/test_algorithm.py
+++ WebCLI/views/test_algorithm.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from ..models import Molecule, Algorithm_version
 from WebCLI.celery.task_sender import send_benchmark_task
 from django.forms.models import model_to_dict
-from WebCLI.models import Metrics
+from WebCLI.models import Metrics, Average_history
 
 
 def test_algorithm(request):
@@ -21,6 +21,11 @@ def test_algorithm(request):
             success_rate=None)
         metrics.save()
         metrics_id = metrics.pk
+        average_history = Average_history(
+            analyzed_results=metrics_id,
+            data=None,
+            iteration_number=None)
+        )
 
     send_benchmark_task(
         metrics_id, model_to_dict(molecule), version.circuit,
--- WebCLI/views/worker_api.py
+++ WebCLI/views/worker_api.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from ..models import Metrics
+from ..models import Metrics, Average_history
 from django.utils import timezone
 import json
 
@@ -14,6 +14,14 @@ def as_analyzed_results(result):
     metrics.success_rate = result["success_rate"]
     return metrics
 
+def as_average_history(result):
+    average_history = Average_history.objects.get(pk=result["metrics_id"])
+    metrics.qubit_count = result["qubit_count"]
+    metrics.timestamp = timezone.now()
+    metrics.gate_depth = result["gate_depth"]
+    metrics.average_iterations = result["average_iterations"]
+    metrics.success_rate = result["success_rate"]
+    return metrics
 
 # TODO: set this route to accept from workers only
 @csrf_exempt
