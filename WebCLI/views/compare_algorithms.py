8ff98e31b4e16220d74ccdddf95fa47e5812ed8b
Tapani Honkanen <tapani.honkanen@hotmail.com>
2021-05-02 12:48:30 +0300

    Reduced complexity

--- WebCLI/views/compare_algorithms.py
+++ WebCLI/views/compare_algorithms.py
@@ -23,9 +23,9 @@ class AlgorithmComparisonView(AlgorithmViewBase):
         selected_metrics2 = self.get_selected_metrics(params["metrics2_id"], metrics2)
         return [selected_metrics1, selected_metrics2]
 
-    def _get_common_molecules(self, version1, version2):
-        molecules1 = Metrics.objects.filter(algorithm_version=version1).values("molecule")
-        molecules2 = Metrics.objects.filter(algorithm_version=version2).values("molecule")
+    def _get_common_molecules(self, versions):
+        molecules1 = Metrics.objects.filter(algorithm_version=versions[0]).values("molecule")
+        molecules2 = Metrics.objects.filter(algorithm_version=versions[1]).values("molecule")
         common_molecules = Molecule.objects.filter(pk__in=molecules1.intersection(molecules2))
         return common_molecules
 
@@ -41,6 +41,12 @@ class AlgorithmComparisonView(AlgorithmViewBase):
         selected_molecule = common_molecules[0]
         params["molecule_id"] = selected_molecule.pk
         return selected_molecule
+
+    def _update_params(self, params, selected_metrics_list, selected_versions):
+        params["metrics1_id"] = selected_metrics_list[0].pk if selected_metrics_list[0] else None
+        params["metrics2_id"] = selected_metrics_list[1].pk if selected_metrics_list[1] else None
+        params["version1_id"] = selected_versions[0].pk if selected_versions[0] else None
+        params["version2_id"] = selected_versions[1].pk if selected_versions[1] else None
 
     def get(self, request, a1_id, a2_id):
         algorithms = Algorithm.objects.filter(pk=a1_id) | Algorithm.objects.filter(pk=a2_id)
@@ -61,12 +67,9 @@ class AlgorithmComparisonView(AlgorithmViewBase):
         )
         (metrics1, metrics2) = self._get_all_metrics(params, versions1, versions2)
         selected_metrics_list = self._get_all_selected_metrics(params, metrics1, metrics2)
-        params["metrics1_id"] = selected_metrics_list[0].pk if selected_metrics_list[0] else None
-        params["metrics2_id"] = selected_metrics_list[1].pk if selected_metrics_list[1] else None
-        (av1, av2) = self._get_selected_versions(params, versions1, versions2)
-        params["version1_id"] = av1.pk if av1 else None
-        params["version2_id"] = av2.pk if av2 else None
-        common_molecules = self._get_common_molecules(av1, av2)
+        selected_versions = self._get_selected_versions(params, versions1, versions2)
+        common_molecules = self._get_common_molecules(selected_versions)
+        self._update_params(params, selected_metrics_list, selected_versions)
         selected_molecule = self._get_selected_molecule(params, common_molecules)
         average_history_graph_data = self.get_history_graph_data(
             Average_history, selected_metrics_list
@@ -76,7 +79,8 @@ class AlgorithmComparisonView(AlgorithmViewBase):
         )
 
         return render(request, 'WebCLI/compareAlgorithms.html',
-                      {'params': params, 'a1': a1, 'av1': av1, 'a2': a2, 'av2': av2,
+                      {'params': params, 'a1': a1, 'av1': selected_versions[0],
+                       'a2': a2, 'av2': selected_versions[1],
                        'metrics1': metrics1, 'metrics2': metrics2,
                        'selected_metrics1': selected_metrics_list[0],
                        'selected_metrics2': selected_metrics_list[1],
